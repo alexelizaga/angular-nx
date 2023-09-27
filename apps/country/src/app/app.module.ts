b053e5acaf224ab183dd68e2690dc7afb3a072d2
alexelizaga <alexelizaga@icloud.com>
2023-09-27 19:30:44 +0200

    country

--- apps/country/src/app/app.module.ts
+++ apps/country/src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [AppRoutingModule, BrowserModule, HttpClientModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
