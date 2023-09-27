b49f47d52302385f168b745b47b2c43f3a4f4ae3
alexelizaga <alexelizaga@icloud.com>
2023-09-27 19:44:33 +0200

    gifs

--- apps/gifs/src/app/app.module.ts
+++ apps/gifs/src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { GifsModule } from './gifs/gifs.module';
+import { SharedModule } from './shared/shared.module';
+
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, GifsModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
