c1227f11007a2261a8b9b8814b6de401e917c8c5
alexelizaga <alexelizaga@icloud.com>
2023-09-27 18:47:43 +0200

    pipes

--- apps/pipes/src/app/app.module.ts
+++ apps/pipes/src/app/app.module.ts
@@ -1,12 +1,29 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+
+import { SharedModule } from './shared/shared.module';
+
+import localeES from '@angular/common/locales/es';
+import localeFr from '@angular/common/locales/fr';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeES);
+registerLocaleData(localeFr);
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+
+    SharedModule,
+  ],
+  providers: [{ provide: LOCALE_ID, useValue: 'es' }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
